4319c3258bc3a034576402adae44b82a4bbca58f
SophieH57 <marinsophie91@yahoo.fr>
2023-07-01 17:15:19 +0200

    envoi requete microservice2

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import router from './routes.js';
+import cors from 'cors';
 
 dotenv.config({path:'./.env'})
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json(), router);
+app.use(express.json(), router, cors());
 
 app.listen(port, ()=> {
     console.log(`Server is running at http://localhost:${port}`);
--- routes.js
+++ routes.js
@@ -1,50 +1,69 @@
-import express from 'express';
-import axios from 'axios';
+import express from "express";
+import axios from "axios";
+import dotenv from "dotenv";
 
-const router = express.Router()
-const microservice1 = process.env.URL_COMPANY_BILLING;
-const microservice2 = process.env.URL_COMPANY;
-const microservice3 = process.env.URL_COMPANY_CUSTOMERS;
-const microservice4 = process.env.URL_PRODUCTS;
-const microservice5 = process.env.URL_FINAL_CUSTOMER;
-const microservice6 = process.env.URL_FINAL_CUSTOMERS_BILLING;
-const microservice7 = process.env.URL_COMMAND;
+dotenv.config({ path: "./.env" });
 
-router.post('/company_billing', async (request, response)=>{
-    if (request.body){
-        try{
-            res = await axios.post(microservice1, request.body);
-            response.status(200).json(res.data);
-        }
-        catch(error){
-            response.status(500).json({error})
-        }
+const router = express.Router();
+const microservice1 = process.env.URL_COMPANY_INVOCING_SERVICE;
+const microservice2 = process.env.URL_COMPANY_SERVICE;
+const microservice3 = process.env.URL_COMPANY_USERS_SERVICE;
+const microservice4 = process.env.URL_PRODUCTS_SERVICE;
+const microservice5 = process.env.URL_CUSTOMER_SERVICE;
+const microservice6 = process.env.URL_CUSTOMERS_INVOICING_SERVICE;
+const microservice7 = process.env.URL_ORDER_SERVICE;
+
+const header = {
+  headers: {
+    "X-Custom-Header": "Showcase-gateway-API-data",
+  },
+};
+
+console.log(microservice2);
+
+router.post("/company_invocing", async (request, response) => {
+  if (request.body) {
+    try {
+      (res = await axios.post(microservice1, request.body)), header;
+      response.status(200).json(res.data);
+    } catch (error) {
+      response.status(500).json({ error });
     }
-})
+  }
+});
 
-router.post('/company',(request, response)=> {
-    console.log(request.body);
-    response.status(200).send('requête envoyée')
-})
-router.post('/company_customers',(request, response)=> {
-    console.log(request.body);
-    response.status(200).send('requête envoyée')
-})
-router.post('/product',(request, response)=> {
-    console.log(request.body);
-    response.status(200).send('requête envoyée')
-})
-router.post('/final_customer',(request, response)=> {
-    console.log(request.body);
-    response.status(200).send('requête envoyée')
-})
-router.post('/final_customers_billing',(request, response)=> {
-    console.log(request.body);
-    response.status(200).send('requête envoyée')
-})
-router.post('/command',(request, response)=> {
-    console.log(request.body);
-    response.status(200).send('requête envoyée')
-})
+router.post("/company", async (request, response) => {
+  if (request.body) {
+    try {
+      res = await axios.post(`${microservice2}/create`, request.body, header);
+      res.status(200)
+        ? response.status(200).send("ok")
+        : response.status(500).send("problem");
+      response.status(200).json(res.data);
+      console.log(response.data);
+    } catch (error) {
+      response.status(500).json({ error });
+    }
+  }
+  //   console.log(request.body);
+  // response.status(200).send('requête envoyée')
+});
+router.post("/company_users", (request, response) => {});
+router.post("/products", (request, response) => {
+  console.log(request.body);
+  response.status(200).send("requête envoyée");
+});
+router.post("/customer", (request, response) => {
+  console.log(request.body);
+  response.status(200).send("requête envoyée");
+});
+router.post("/customers_invoicing", (request, response) => {
+  console.log(request.body);
+  response.status(200).send("requête envoyée");
+});
+router.post("/order", (request, response) => {
+  console.log(request.body);
+  response.status(200).send("requête envoyée");
+});
 
 export default router;
